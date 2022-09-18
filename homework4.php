e346efcf7820771bc642052a2d10da19f6d113dd
Maxxim1506 <prolars1506@gmail.com>
2022-09-18 17:03:20 +0300

    homework

--- homework4.php
+++ homework4.php
@@ -14,14 +14,29 @@ $firstArr = [
 'five' => [
    'three' => 32,
    'foure' => 5,
-   'five' => 12,
+   'five' => ['12' => 1, '13' => 2],
 ],  
 ];
 
-function counterEll($value)
+function arraySearch($array, $number)
 {
-
+	$resultArray = [];
+	$counter = 1;
+	foreach ($array as $value){
+				if ($counter == $number) {
+					$resultArray[] = $value;
+				}
+					if (is_array($value)) {
+							$resultArray[] = arraySearch($value, $number);
+					}
+	  $counter++;
+	}
+	return $resultArray;
 }
+echo '<pre>';
+print_r(arraySearch($firstArr, 2));
+echo '</pre>';
+
 
 $str = 'bbbbbbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbb';
 
@@ -31,7 +46,7 @@ function counterB($str)
 	return substr_count($str, 'b');
 	} else {
 	return false;
-	}
+	}	
 }
 $result = counterB($str);
 var_dump($result);
