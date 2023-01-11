f6e47dccc9745a6d56240d3cb1fececf88ef17c9
Max <prolars1506@gmail.com>
2023-01-11 17:03:45 +0200

    MVC router

---
+++ Advance/App/Models/Car.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+class Car extends \Core\Model
+{
+    protected static string|null $tableName = 'cars';
+
+    public function getInfo(): string
+    {
+        // $this->name
+        // $this?->name ===== if $this === null ? null : $this->name
+        return $this?->model . ' - ' . $this?->price;
+    }
+
+}
